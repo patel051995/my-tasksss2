336290d002e7d15503ab210ea13e2022b1c0cda2
Vivek <Vivek9993227878@gmail.com>
2024-09-21 09:21:06 +0000

    Solution

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import {Component} from 'react'
-import {v4 as uuid} from 'uuid'
-import Tasks from './components/Tasks'
+import Home from './components/Home'
+import './App.css'
 
-import {
-  MainContainer,
-  TasksInputContainer,
-  TasksDisplayContainer,
-  Heading,
-  InputContainer,
-  LabelText,
-  Input,
-  Select,
-  AddButton,
-  TagsHeading,
-  TagsContainer,
-  TasksContainer,
-  TagsButton,
-  TagListItem,
-  NoTaskText,
-} from './style'
+const App = () => <Home />
+
+export default App
+import MyTask from './component/MyTask'
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
 
 const tagsList = [
   {
@@ -47,118 +36,7 @@ const tagsList = [
   },
 ]
 
-class App extends Component {
-  state = {
-    myTaskList: [],
-    inputTask: '',
-    selectTag: tagsList[0].optionId,
-    activeTag: 'INITIAL',
-  }
-
-  onClickAddButton = () => {
-    const {inputTask, selectTag} = this.state
-    const taskName = inputTask
-    const TasksCategory = selectTag
-    const id = uuid()
-    const bgColor = false
-
-    if (taskName.length == 0) {
-      this.setState(prevState => ({
-        myTaskList: [
-          ...prevState.myTaskList,
-          {id, taskName, TasksCategory, bgColor},
-        ],
-        inputTask: '',
-        selectTag: tagsList[0].optionId,
-      }))
-    }
-  }
-
-  onChangeInputTask = event => {
-    this.setState({inputTask: event.target.value})
-  }
-
-  onChangeSelectTag = event => {
-    this.setState({selectTag: event.target.value})
-  }
-
-  onClickTag = event => {
-    this.setState(prevState => ({
-      activeTag:
-        prevState.activeTag == event.target.value
-          ? 'INITIAL'
-          : event.target.value,
-    }))
-  }
-
-  render() {
-    const {myTaskList, inputTask, selectTag, activeTag} = this.state
-    const filterTaskList =
-      activeTag == 'INITIAL'
-        ? myTaskList
-        : myTaskList.filter(each => each.TasksCategory == activeTag)
-
-    return (
-      <MainContainer>
-        <TasksInputContainer>
-          <Heading>Create a Tasks</Heading>
-          <InputContainer>
-            <LabelText for="textInput">Task</LabelText>
-            <Input
-              id="textInput"
-              type="text"
-              placeholder="Enter the task here"
-              value={inputTask}
-              onnChange={this.onChangeInputTask}
-            />
-            <LabelText for="optionInput">Tags</LabelText>
-            <Select
-              id="optionInput"
-              value={selectTag}
-              onChange={this.onChangeSelectTag}
-            >
-              {tagsList.map(eachTag => (
-                <option value={eachTag.optionId}>{eachTag.displayText}</option>
-              ))}
-            </Select>
-          </InputContainer>
-          <AddButton type="button" onClick={this.onClickAddButton}>
-            Add Task
-          </AddButton>
-        </TasksInputContainer>
-        <TasksDisplayContainer>
-          <TagsHeading>Tags</TagsHeading>
-          <TagsContainer>
-            {tagsList.map(eachTag => {
-              const isActive = activeTag == eachTag.optionId
-              return (
-                <TagListItem key={eachTag.optionId}>
-                  <TagsButton
-                    type="button"
-                    value={eachTag.optionId}
-                    onClick={this.onClickTag}
-                    isActive={isActive}
-                  >
-                    {eachTag.displayText}
-                  </TagsButton>
-                </TagListItem>
-              )
-            })}
-          </TagsContainer>
-          <TagsHeading>Tasks</TagsHeading>
-          <TasksContainer>
-            {filterTaskList.length == 0 ? (
-              <NoTaskText>No Task Added Yet</NoTaskText>
-            ) : (
-              filterTaskList.map(eachTask => (
-                <Tasks key={eachTask.id} taskDetails={eachTask} />
-              ))
-            )}
-          </TasksContainer>
-        </TasksDisplayContainer>
-      </MainContainer>
-    )
-  }
-}
+// Replace your code here
+const App = () => <MyTask tagsList={tagsList} />
 
 export default App
